1becaa31ddfa64f3016f7f6aed4bd56313a2e55e
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-05-05 14:11:46 -0300

    1ERA ENTREGA PROYECTO FINAL: Creando objeto viaje.

---
+++ 1era entrega Proyecto Final/script.js
@@ -0,0 +1,9 @@
+function Viaje (destino, clase, escala) {
+    this.destino = destino;
+    this.clase = clase;
+    this.escala = escala;
+    precioDestino = 0;
+    precioClase = 0;
+    precioEscala = 0;
+    precioTotal = 0;
+}
