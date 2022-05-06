abf082b23dda5a23b71fcce6dfd91f95daeacdd6
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-05-05 22:02:51 -0300

    1ERA ENTREGA PROYECTO FINAL: Se crea objeto con todos sus metodos, se crea funcion para cantidad de pasajes, se crea arreglo de pasajes. Se crea funcion para instanciar objeto y se crea funcion para cargar pasajes al arreglo depende la cantidad de pasajes que seleccione el usuario.

--- 1era entrega Proyecto Final/script.js
+++ 1era entrega Proyecto Final/script.js
@@ -1,9 +1,139 @@
-function Viaje (destino, clase, escala) {
-    this.destino = destino;
-    this.clase = clase;
-    this.escala = escala;
-    precioDestino = 0;
-    precioClase = 0;
-    precioEscala = 0;
-    precioTotal = 0;
+//Empresa de venta de pasajes de Aerolinea
+
+//OBJETOS
+
+class Pasaje {
+    constructor () {
+        this.destino = '';
+        this.clase = '';
+        this.escala = '';
+        this.precioDestino = 0;
+        this.precioClase = 0;
+        this.precioEscala = 0;
+        this.precioTotal = 0;
+    }
+
+    getDestino(){
+        return (this.destino);
+    }
+    getClase(){
+        return (this.clase);
+    }
+    getEscala(){
+        return (this.escala);
+    }
+    getPrecioDestino(){
+        return (this.precioDestino);
+    }
+    getPrecioClase(){
+        return (this.precioClase);
+    }
+    getPrecioEscala(){
+        return (this.precioEscala);
+    }
+    getPrecioTotal(){
+        return (this.precioTotal);
+    }
+
+    setDestino(){
+        let destino = (prompt('Elija el lugar donde desea viajar: Venecia, Madrid, Londres, Miami')).toLowerCase();
+        switch (destino) {
+            case 'venecia':
+                this.destino = 'Venecia';
+                this.precioDestino = 90000;
+                break;
+            case 'madrid':
+                this.destino = 'Madrid';
+                this.precioDestino = 110000;
+                break;
+            case 'londres':
+                this.destino = 'Londres';
+                this.precioDestino = 95000;
+                break;
+            default:
+                this.destino = 'Miami';
+                this.precioDestino = 70000;
+                break;
+        }
+    }
+    setClase(){
+        let clase = (prompt('Elija el tipo de asiento en el que desea viajar: turista, premium, ejecutiva, primera clase')).toLowerCase();
+        switch (clase) {
+            case 'turista':
+                this.clase = 'Turista';
+                this.precioClase = 0;
+                break;
+            case 'premium':
+                this.clase = 'Premium';
+                this.precioClase = 5000;
+                break;
+            case 'ejecutiva':
+                this.clase = 'Ejecutiva';
+                this.precioClase = 9000;
+                break;
+            default:
+                this.clase = 'Primera Clase';
+                this.precioClase = 15000;
+                break;
+        }
+    }
+    setEscala(escala){
+        let aux = (prompt('Seleccione si desea que su viaje tenga escalas o sea directo. Elija SI para que tenga escala o NO para que sea directo.')).toLowerCase();
+        if (aux == 'si') {
+            this.escala = 'Si';
+            this.precioEscala = 0;
+        } else {
+            this.escala = 'No';
+            this.precioEscala = 12000;
+        }
+    }
+
+    calcularCostoTotal (){
+        this.precioTotal = this.precioDestino + this.precioClase + this.precioEscala;
+    }
 }
+
+//ARREGLOS
+
+const arregloPasajes = [];
+
+//VARIABLES
+
+let cantPasajes = 0;
+
+//FUNCIONES
+
+
+//Genera un pasasje
+function generarPasaje(){
+    alert('Gracias por utilizar AirWolrd. Nos alegra atenderlo.');
+
+    const pasaje = new Pasaje();
+    
+    pasaje.setDestino();
+
+    pasaje.setClase();
+
+    pasaje.setEscala();
+
+    pasaje.calcularCostoTotal();
+
+    cantidadPasajes(cantPasajes,pasaje);
+}
+
+//Solicita que el usuario ingrese la cantidad de pasajes de que desea comprar
+function cantidadPasajes(cantPasajes, pasaje){
+    cantPasajes = parseInt(prompt('Ingrese con un número la cantidad de pasajes que desea comprar.'));
+
+    for (i = cantPasajes; i > 0; i--){
+        arregloPasajes.push(pasaje);
+    }
+
+    console.log(arregloPasajes);
+    console.log(arregloPasajes[1]);
+}
+
+//PROGRAMA PRINCIPAL
+
+generarPasaje();
+ 
