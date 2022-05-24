5dff50283084a77ccb10a740a2d477f091ab747d
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-05-24 20:30:32 -0300

    OPTIMIZANDO EL PROYECTO FINAL: Se aplica operadores ternarios, nullify y desestructuración en el código. No hay ninguna copia de elementos como para usar 'SPREAD' aún.

--- Optimizando el proyecto final/js/scripts.js
+++ Optimizando el proyecto final/js/scripts.js
@@ -23,40 +23,40 @@ let productos = [producto1, producto2, producto3, producto4, producto5, producto
 //Creo el array donde se van a guardar los productos en el carrito
 let carrito = [];
 
+/*Consulto en mi localStorage si existe mi item carrito. Si el carrito no existe me va a devolver null.
+Se crea en esta instancia porque más adelante voy a consultar el carrito.
+Si es TRUE:Si existe el carrito en localStorage, entonces mi carrito va a ser igual que el carrito que está en localStorage. Si tengo productos ahi adentro los voy a recuperar. Como el carrito del storage va a estar en formato json lo voy a tener que convertir con la funcion JSON.parse. Si es resultado es null porque no hay objetos dentro del storage, entonces mediante el nullify '??' le asigno un array vacio.
+Si es FALSE: Si el carrito no existe, lo creo y convierto el objeto a JSON con el stringify.*/
 
-
-//Consulto en mi localStorage si existe mi item carrito. Si el carrito no existe me va a devolver null.
-//Se crea en esta instancia porque más adelante voy a consultar el carrito.
-if(localStorage.getItem('carrito')){ //Si no existe, esto da null.
-    //Si existe el carrito en localStorage, entonces mi carrito va a ser igual que el carrito que está en localStorage. Si tengo productos ahi adentro los voy a recuperar. Como el carrito del storage va a estar en formato json lo voy a tener que convertir con la funcion JSON.parse.
-    carrito = JSON.parse(localStorage.getItem('carrito'));
-} else {
-    //Si el carrito no existe, lo creo y convierto el objeto a JSON con el stringify.
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-}
+//UTILIZO OPERADOR TERNARIO Y NULLIFY
+(localStorage.getItem('carrito')) ? (carrito = JSON.parse(localStorage.getItem('carrito')) ?? []) : localStorage.setItem('carrito', JSON.stringify(carrito));
 
 let contModelos = document.querySelector('#contModelos');
 let cantCarrito = document.querySelector('#cantCarrito');
-
-productos.forEach((producto, i) => {
-    contModelos.innerHTML += `
-        <div id="producto${i}" class="card modelos__producto col-12 col-md-3 mx-3">
-            <img src="./img/zapatilla${i}.webp" class="card-img-top" alt="Imagen de zapatilla ${i}">
-            <div class="card-body">
-                <h5 class="card-title">${producto.marca} ${producto.modelo}</h5>
-                <p class="card-text">$${producto.precio}</p>
-                <p class="card-text">${producto.stock} unidades</p>
-                <button id="botonProducto${i}" class="btn btn-danger modelos__producto__boton">Agregar al carrito</button>
-            </div>
-        </div>
-    `
-})
 
 //Coloca la cantidad inicial de items en el carrito del localStorage al momento de iniciar la página.
 cantCarrito.innerHTML = `
     <i class="fa-solid fa-cart-shopping"></i>
     <span>${carrito.length}</span>
 `
+
+//Creo las cards de cada producto.
+productos.forEach((producto, i) => {
+    //UTILIZO DESESTRUCTURACIÓN
+    let {marca, modelo, precio, stock} = producto;
+    contModelos.innerHTML += `
+        <div id="producto${i}" class="card modelos__producto col-12 col-md-3 mx-3">
+            <img src="./img/zapatilla${i}.webp" class="card-img-top" alt="Imagen de zapatilla ${i}">
+            <div class="card-body">
+                <h5 class="card-title">${marca} ${modelo}</h5>
+                <p class="card-text">$${precio}</p>
+                <p class="card-text">${stock} unidades</p>
+                <button id="botonProducto${i}" class="btn btn-danger modelos__producto__boton">Agregar al carrito</button>
+            </div>
+        </div>
+    `
+})
+
 
 //Consulto cada botón de cada producto y le agrego un evento escuchador de tipo 'click'. Cuando se clickea un botón retorna el objeto producto correspondiente a ese botón.
 productos.forEach((producto,i) => {
@@ -77,13 +77,6 @@ productos.forEach((producto,i) => {
     });
 })
 
-carrito.forEach((producto,i) => {
-    
-})
-
-//DE ESTA MANERA SE PODRÍA CREAR UN BOTÓN PARA ELIMINAR UN ITEM DEL CARRITO (AGREGAR FUNCIONALIDAD EN PRÓXIMA ENTREGA). Minuto 1:36:00 del video Workshop I.
-// document.querySelector(`#producto${i}`).remove();
-//carrito.splice(i,1)
 
 let formularioC = document.querySelector('#formularioContacto');
 
@@ -102,4 +95,11 @@ formularioC.addEventListener('submit', (event) => {
     formularioC.reset();
 })
 
+/* CÓDIGO QUE SE USARÁ PARA LA PÁGINA DEL CARRITO */
 
+//DE ESTA MANERA SE PODRÍA CREAR UN BOTÓN PARA ELIMINAR UN ITEM DEL CARRITO (AGREGAR FUNCIONALIDAD EN PRÓXIMA ENTREGA). Minuto 1:36:00 del video Workshop I.
+// document.querySelector(`#producto${i}`).remove();
+//carrito.splice(i,1)
+
+
+/* CÓDIGO QUE SE USARÁ PARA LA PÁGINA DE CADA PRODUCTO INDIVIDUAL */
